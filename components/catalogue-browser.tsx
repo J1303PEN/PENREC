f39a61c8515ce7643afb8df6991b1212e051e9a0
J1303PEN <darrenepenman@me.com>
2026-09-15 15:12:07 +0100

    Add Christie Walker to catalogue browser

--- components/catalogue-browser.tsx
+++ components/catalogue-browser.tsx
@@ -8,12 +8,13 @@ import { theParkers } from "@/data/parkers";
 import { maison45 } from "@/data/maison-45";
 import { localArrangement } from "@/data/local-arrangement";
 import { directMotion } from "@/data/direct-motion";
+import { christieWalker } from "@/data/christie-walker";
 import { saturdayBest } from "@/data/saturday-best";
 import { getCatalogueReleaseArtists } from "@/data/releases";
 
 const filters = ["All", "United Kingdom", "Italy", "Germany", "Canada", "International"] as const;
 const catalogueArtists = [
-  ...getCatalogueReleaseArtists([...artists, theVerelles, theParkers, maison45, localArrangement, directMotion]),
+  ...getCatalogueReleaseArtists([...artists, theVerelles, theParkers, maison45, localArrangement, directMotion, christieWalker]),
   {...saturdayBest, releaseHref: `/releases/${saturdayBest.slug}`},
 ];
 
