77622f5760b1659637a7a5298bd51e1418a3291c
J1303PEN <darrenepenman@me.com>
2026-09-18 17:13:46 +0100

    Force homepage to read current Studio catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { HomepageContent } from "@/components/homepage-content";
 import { getPublicCatalogueReleases } from "@/lib/catalogue-live";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function HomePage() {
   const releases=await getPublicCatalogueReleases();
   return <HomepageContent releases={releases}/>;
